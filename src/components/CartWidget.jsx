ecf3169a23213f58c45717d3a080cf5cbf087582
Mariano Romero <marianoromero.11@gmail.com>
2024-08-27 22:51:01 -0300

    pre-entrega-01

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+import cartIcon from '../assets/shopping_cart_icon.png';
+
+function CartWidget () 
+{
+  return (
+    <div>
+      <img src={cartIcon} alt="Cart icon" />
+      <span>3</span>
+    </div>
+  );
+};
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+function ItemListContainer({ message })
+{
+    return (
+        <div>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default ItemListContainer;
